072be05344448d2a3b3b6a98f297b53de18eb406
Евгений <101981923+Bobishe@users.noreply.github.com>
2025-07-08 15:04:38 +0700

    Deduplicate D3 modules in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['d3-selection', 'd3-transition', 'd3-zoom']
+  },
   server: {
     port: 3000
   }
