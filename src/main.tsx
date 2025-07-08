18debc82da716a28f5a2a52bbd50938bb2d4d704
Евгений <101981923+Bobishe@users.noreply.github.com>
2025-07-08 14:13:21 +0700

    Fix ReactFlow styles and container size

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
 import './index.css'
+import 'reactflow/dist/style.css'
 import { ReactFlowProvider } from 'reactflow'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
