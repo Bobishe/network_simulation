a6075e7847f5a7e5db8c093778e4ed1e67b18327
eoskolkov <zenya.oskolkov2001@gmail.com>
2025-07-09 11:01:50 +0700

    fix sat-net

--- src/components/PaletteBar.tsx
+++ src/components/PaletteBar.tsx
@@ -1,32 +1,31 @@
-import PaletteButton from './PaletteButton'
+import PaletteButton from "./PaletteButton";
 import {
   CubeIcon,
   CubeTransparentIcon,
   HomeModernIcon,
   ArrowRightIcon,
-} from '@heroicons/react/24/solid'
+} from "@heroicons/react/24/solid";
 
 const items = [
-  { type: 'leo', icon: CubeIcon, label: 'LEO' },
-  { type: 'meo', icon: CubeTransparentIcon, label: 'MEO' },
-  { type: 'geo', icon: CubeIcon, label: 'GEO', ring: true },
-  { type: 'gnd', icon: HomeModernIcon, label: 'GND' },
-  { type: 'link', icon: ArrowRightIcon, label: 'LINK' },
-]
+  { type: "leo", icon: CubeIcon, label: "LEO" },
+  { type: "meo", icon: CubeTransparentIcon, label: "MEO" },
+  { type: "geo", icon: CubeIcon, label: "GEO", ring: true },
+  { type: "gnd", icon: HomeModernIcon, label: "GND" },
+  { type: "link", icon: ArrowRightIcon, label: "LINK" },
+];
 
 export default function PaletteBar() {
   return (
     <div className="sticky left-0 top-0 z-10 bg-white flex flex-col items-center p-2 h-full w-20 border-r">
-      <div className="font-bold mb-4">SAT-NET</div>
-      {items.map(item => (
+      {items.map((item) => (
         <PaletteButton
           key={item.type}
           icon={item.icon}
           label={item.label}
           type={item.type}
-          ring={'ring' in item ? item.ring : undefined}
+          ring={"ring" in item ? item.ring : undefined}
         />
       ))}
     </div>
-  )
+  );
 }
