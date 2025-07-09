ed3b1716699047f130b917fb0046ab99f0b0bd9d
Евгений <101981923+Bobishe@users.noreply.github.com>
2025-07-09 14:38:12 +0700

    style: align delete tool to right

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import { TrashIcon } from '@heroicons/react/24/solid'
 
 export default function TopBar() {
   return (
-    <div className="fixed top-0 left-0 w-full h-12 bg-white border-b flex items-center px-2 z-20">
+    <div className="fixed top-0 left-0 w-full h-12 bg-white border-b flex items-center justify-end px-2 z-20">
       <PaletteButton icon={TrashIcon} label="DEL" type="delete" />
     </div>
   )
